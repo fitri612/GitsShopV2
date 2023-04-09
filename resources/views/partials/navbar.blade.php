b27ecd3be6d22ae5d721f12f195d5dea4a0d7de1
fitri612 <fitringer612@gmail.com>
2023-04-09 23:07:25 +0700

    Update navbar.blade.php
    sidebgar guest

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,40 +1,54 @@
-<nav>
-    <div class="sidebar">
-        <div class="head-text">
-            <h3 class="text-center">user</h3>
-        </div>
-        <div class="menu-link">
-            <a href="{{ route('transaction.cashier') }}"><i class="fa fa-cart-shopping " aria-hidden="true"></i> Casier</a>
-            <a href="{{ route('transaction.history') }}"><i class="fa-solid fa-cart-plus"></i> Transaksi</a>
-        </div>
-        <a href="{{ route('show_profile') }}"><i class="fa-solid fa-user"></i>Profile</a>
-        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-        document.getElementById('logout-form').submit();">
-        <i class="fa-sharp fa-solid fa-right-from-bracket"></i>{{ __('Logout') }}</a>
-            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                @csrf
-            </form>
-    </div>
-    <div class="nav-content">
-        <div class="logo">
-            <img src="../img/keyboard.png" alt="" style="width:60px;height:60px;">
-            <a href="/" style="color:#00214d">Lemon Shop</a>
-        </div>
-        <form class="search-box">
-            <input type="text" id="searchInput" onkeyup="searchByName()" placeholder="Search by name...">
-            <button type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
-        </form>
-        <ul class="nav-links">
-            @guest
-            @if (Route::has('login'))
-                    <li><a href="{{ route('login') }}"><button class="btn btn-primary">masuk</button></a></li>
-                @endif
+<header>
+    <nav id="sidebarMenu" class="collapse d-lg-block sidebar collapse bg-white">
+        <div class="position-sticky">
+            <div class="list-group list-group-flush mx-3 mt-4">
+                <a href="#" class="list-group-item list-group-item-action py-2 ripple" aria-current="true">
+                    <i class="fas fa-tachometer-alt fa-fw me-3"></i><span>Main dashboard</span>
+                </a>
+                <a href="{{ route('transaction.cashier') }}"
+                    class="list-group-item list-group-item-action py-2 ripple active">
+                    <i class="fas fa-chart-area fa-fw me-3"></i><span>Cashier</span>
+                </a>
+                <a href="{{ route('transaction.history') }}"
+                    class="list-group-item list-group-item-action py-2 ripple"><i
+                        class="fas fa-lock fa-fw me-3"></i><span>Transaction</span></a>
+                <a href="{{ route('show_profile') }}" class="list-group-item list-group-item-action py-2 ripple"><i
+                        class="fas fa-chart-line fa-fw me-3"></i><span>Profile</span></a>
+                <li class="nav-item dropdown" style="list-style-type: none;">
+                    <a class="nav-link dropdown-toggle hidden-arrow d-flex align-items-center"
+                        href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                    document.getElementById('logout-form').submit();">
+                        <i class="fas fa-chart-line fa-fw me-3"></i><span>{{ __('Logout') }}</span>
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
 
-                @if (Route::has('register'))
-                    <li><a href="{{ route('register') }}"><button class="btn btn-primary">daftar</button></a></li>
-                @endif
                 </li>
-            @endguest
-        </ul>
-    </div>
-</nav>
+            </div>
+        </div>
+    </nav>
+    <nav id="main-navbar" class="navbar navbar-expand-lg navbar-light bg-white fixed-top">
+        <!-- Container wrapper -->
+        <div class="container-fluid">
+            <!-- Toggle button -->
+            <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#sidebarMenu"
+                aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fas fa-bars"></i>
+            </button>
+
+            <!-- Brand -->
+            <a class="navbar-brand" href="#">
+                <img src="../img/keyboard.png" style="width:60px;height:60px;" loading="lazy" />
+                <strong class="ms-1">Lemon Shop</strong>
+            </a>
+            <!-- Search form -->
+            <form class="d-none d-md-flex input-group w-auto my-auto">
+                <input type="text" id="searchInput" onkeyup="searchByName()" placeholder="Search by name...">
+                <span class="input-group-text border-0"><i class="fas fa-search"></i></span>
+            </form>
+        </div>
+        <!-- Container wrapper -->
+    </nav>
+</header>
