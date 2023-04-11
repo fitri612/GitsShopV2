763dd6f5b14e16cda11d37fc064e01f91063bec2
Aldi <20sa1132@amikompurwokert.ac.id>
2023-04-11 13:12:11 +0700

    membuat seeder

--- database/seeders/productSeeder.php
+++ database/seeders/productSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class productSeeder extends Seeder
 {
@@ -14,6 +15,36 @@ class productSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('categories')->insert([
+            [
+                'id' => 1,
+                'name' => 'komputer',
+            ],
+            [
+                'id' => 2,
+                'name' => 'laptop',
+            ]
+        ]);
+
+        DB::table('products')->insert([
+            [
+                'id' => 1,
+                'name' => 'Pc Gaming',
+                'price' => 5000000,
+                'description' => 'sebuah komputer dengan spesifikasi tinggi',
+                'category_id' => 1,
+                'image' => '../img/pc.png',
+                'stock' => 10
+            ],
+            [
+                'id' => 2,
+                'name' => 'laptop',
+                'price' => 5000000,
+                'description' => 'sebuah laptop dengan spesifikasi tinggi',
+                'category_id' => 2,
+                'image' => '../img/laptop.png',
+                'stock' => 10
+            ]
+        ]);
     }
 }
