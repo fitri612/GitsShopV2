cb90549bbbb491508fdb6d647e322f844079a3d9
Lailatul Fitriyah <74086121+fitri612@users.noreply.github.com>
2023-04-12 16:54:42 +0700

    remove unused script admin

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -56,6 +56,6 @@
     {{-- bootstrap --}}
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/jquery-match-height@0.7.2/dist/jquery.matchHeight.min.js"></script>
-    <script src="{{ asset('js/control.js') }}"></script>
-    <script src="{{ asset('js/overlay.js') }}"></script>
+    <!-- <script src="{{ asset('js/control.js') }}"></script>
+    <script src="{{ asset('js/overlay.js') }}"></script> -->
     </html>
